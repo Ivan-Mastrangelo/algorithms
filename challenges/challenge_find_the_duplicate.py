15108a37190a90f15af02f4128a119440d7b12c2
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-09-05 16:02:47 -0300

    feat: implementa função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
+    if len(nums) < 2 or nums is None:
+        return False
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if isinstance(nums[i], str) or nums[i] < 0:
+            return False
+        elif nums[i] == nums[i - 1]:
+            return nums[i]
+    return False
