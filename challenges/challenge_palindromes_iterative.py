4b5219a0054a6163585c047a2e0a645665c03fb4
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-09-05 18:33:35 -0300

    feractor: refatorar algorítmo para diminuir a complexidade assintótica

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,10 +2,9 @@ def is_palindrome_iterative(word):
     """Faça o código aqui."""
     if not word:
         return False
-    while len(word) >= 2:
-        last_index = len(word) - 1
-        if word[0] == word[last_index]:
-            word = word[1:last_index]
-        else:
-            return False
-    return True
+
+    reversed_word = word[::-1]
+    if word == reversed_word:
+        return True
+    else:
+        return False
